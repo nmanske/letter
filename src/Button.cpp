798eedcab2d48ee5622d6497068ac4c4699bd524
Nathan Manske <nathan.manske@utexas.edu>
2017-11-08 20:22:08 -0600

    Add initial button files

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,24 +1,11 @@
+#include <Arduino.h>
+
 #define LEFT_BTN 2
 #define RIGHT_BTN 3
 #define JUMP_BTN 4
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
-void setup() {
-  pinMode(buttonPin, INPUT);
-}
-
-void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
+void initButton() {
+    pinMode(LEFT_BTN, INPUT);
+    pinMode(RIGHT_BTN, INPUT);
+    pinMode(JUMP_BTN, INPUT);
 }
